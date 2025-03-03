8068b4162cc3337c79aa59151c05d3aed80888d7
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-03-04 01:13:30 +0800

    week 1 code

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Intrinsics.X86;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,13 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // my code - begin
+        var result = new double[length];
+        for (int i = 0; i < length; ++i) {
+            result[i] = number * (i+1); // replace this return statement with your own
+        } 
+        return result;
+        // my code - end
     }
 
     /// <summary>
@@ -29,5 +37,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        // my code - begin
+        for (var i = 0; i < amount; i++) {
+            var lastElement = data[data.Count-1];
+            data.RemoveAt(data.Count-1);
+            data.Insert(0, lastElement);
+        }
+        // my code - end
     }
 }
